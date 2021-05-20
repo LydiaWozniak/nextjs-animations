86592dabb54cbbde212230f7da73089c30eddbad
lydiawozniak <44431046+LydiaWozniak@users.noreply.github.com>
2021-05-20 09:30:04 +0100

    Update gsap

--- components/animations/Button/Button.js
+++ components/animations/Button/Button.js
@@ -34,9 +34,25 @@ export default function Button() {
                 {buttonText}
             </button >
 
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" height="25px" width="25px"
+                viewBox="0, 0, 100, 100">
+
+                <clipPath id="tick">
+                    <path stroke-miterlimit="10" stroke-linecap="round" stroke-width="8" stroke="#7DB0D5" fill="none" class="check" d="M70 35 C61.66 45 53.33 55 45 65 40 60.66 35 56.33 30 52 "></path>
+                </clipPath>
+
+                <g clipPath="url(#tick)">
+                    <circle id="circle" fill="#000" cx="50" cy="50" r="44" />
+                    <use clipPath="url(#tick)" xlinkHref="#circle" fill="red" />
+                </g>
+
+            </svg>
+
+
 
         </div >
     )
 }
 
 
+//create a clip path animation for the tick. They can be two separate svgs which are replaced by react...
