9a6b9782482839265f11af203cc777748d59f681
lydiawozniak <44431046+LydiaWozniak@users.noreply.github.com>
2021-06-18 16:51:10 +0100

    Add hide/reveal to animation

--- components/animations/ReactReveal/ReactReveal.js
+++ components/animations/ReactReveal/ReactReveal.js
@@ -1,9 +1,20 @@
 import Zoom from 'react-reveal/Zoom';
+import { useState, Fragment, } from 'react'
 
 export default function ReactReveal() {
+
+    let [appear, setAppear] = useState(true);
+
+    const replayAnimation = () => {
+        setAppear(!appear)
+    }
+
     return (
-        <Zoom>
-            <p>Hello World</p>
-        </Zoom>
+        <>
+            <Zoom when={appear}>
+                <p>Hello World</p>
+            </Zoom>
+            <button onClick={replayAnimation}>{appear ? "Hide" : "Replay"}</button>
+        </>
     )
 }
